e27b1e4aa4f802da88644c7f51073612b156d37c
vidwalk <claudiurediu@gmail.com>
2019-04-24 11:52:32 +0200

    bridge socket start

---
+++ Embedded/bridgeSocket/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using MongoDB.Driver;
+namespace bridgeSocket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var uri = "mongodb+srv://groupZ1:groupZ1@iotzuperteam-no7vb.mongodb.net/admin?retryWrites=true";
+
+			MongoClient mongoClient = new MongoClient(uri);
+			var database = mongoClient.GetDatabase("ClimatizerDB");
+            Console.WriteLine("Connected to the database successfully");
+        }
+    }
+}
