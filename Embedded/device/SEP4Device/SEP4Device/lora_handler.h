78b9ddd17391db2b5976439fc46237ca67c3c406
skorejen <skorejen@gmail.com>
2019-05-01 16:40:41 +0200

    Splitted the C files into more sensor/lora specific

---
+++ Embedded/device/SEP4Device/SEP4Device/lora_handler.h
@@ -0,0 +1,15 @@
+/*
+ * lora_handler.h
+ *
+ * Created: 5/1/2019 3:23:36 PM
+ *  Author: skorejen
+ */ 
+
+
+#ifndef LORA_HANDLER_H_
+#define LORA_HANDLER_H_
+
+void create_lora_connection(UBaseType_t,UBaseType_t);
+
+
+#endif /* LORA_HANDLER_H_ */
