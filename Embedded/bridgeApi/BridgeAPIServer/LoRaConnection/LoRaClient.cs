8a84a10fefd022990b71781dd64b9e7fc2a5a455
Dominika98 <31960418+Dominika98@users.noreply.github.com>
2019-05-05 14:26:16 +0200

    WebAPI

--- Embedded/bridgeApi/BridgeAPIServer/LoRaConnection/LoRaClient.cs
+++ Embedded/bridgeApi/BridgeAPIServer/LoRaConnection/LoRaClient.cs
@@ -1,14 +1,33 @@
-﻿using System.Net.Http;
-using System.Net.WebSockets;
+﻿using WebSocketSharp;
+using System;
 
 namespace BridgeAPIServer.LoRaConnection
 {
     public class LoRaClient {
-        HttpClient client = new HttpClient();
-        
+      
+
+
         public void Run_Actuator()
         {
-           
+            using (var ws = new WebSocket("https://iotnet.teracom.dk/apps/websocket.html?token=vnoRfwAAABFpb3RuZXQudGVyYWNvbS5ka_ct-QZ1DMiwgA-TLGfiomI="))
+            {
+                ws.OnOpen += (sender, e) =>
+                {
+                    ws.Send("Open Window");
+                };
+
+                ws.OnError += (sender, e) => {
+                    Console.WriteLine("An error occured.");
+                };
+
+                ws.OnClose += (sender, e) => {
+                    Console.WriteLine("Connection closed");
+                };
+
+                ws.Connect();
+                ws.Close();
+            }
+
         }
     }
 }
